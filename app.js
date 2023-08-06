39f1163e7ac99ffd21606483f8158b4b81378e1e
Sambhu <sambhunath@recruitcrm.io>
2023-08-06 13:17:24 +0530

    Sending Response from backend using res.end()

--- app.js
+++ app.js
@@ -3,7 +3,24 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    process.exit();
+
+    // type of response we are sending back example ('text/plain', 'text/html', 'application/json' ... etc)
+    // https://developer.mozilla.org/en-US/docs/Glossary/Response_header
+    res.setHeader('Content-Type', 'text/html'); 
+
+    res.write(
+        `<html>
+            <head>
+                <title>My First Page</title>
+            </head>
+            <body>
+                Hi There!
+            </body>
+        </html>`
+    );
+
+    // send the response back to the client
+    res.end(); 
 });
 
 server.listen(3005);
