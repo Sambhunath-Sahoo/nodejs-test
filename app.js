d51d00c5d5b66716d08333d887dd4a3e0d8999b0
Sambhu <sambhunath@recruitcrm.io>
2023-08-07 00:50:59 +0530

    Getting payload and making it usable for processing in nodejs

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 const http = require('http');
-
+const fs = require('fs');
+const { log } = require('console');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
+
     if (url === '/') {
+        res.write(
+            `<html>
+                <body>
+                    <h2>HTML Forms</h2>
+                    <form action="/message" method='POST'>
+                        <label for="fname">First name:</label><br>
+                        <input type="text" id="fname" name="first_name" value="John"><br>
+                        <label for="lname">Last name:</label><br>
+                        <input type="text" id="lname" name="last_name" value="Doe"><br><br>
+                        <input type="submit" value="Submit">
+                    </form> 
+                </body>
+            </html>`
+        );
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
         
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            fs.appendFileSync('message.txt', parsedBody);
+            console.log(parsedBody);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
     }
 
     // type of response we are sending back example ('text/plain', 'text/html', 'application/json' ... etc)
@@ -17,7 +50,7 @@ const server = http.createServer((req, res) => {
                 <title>My First Page</title>
             </head>
             <body>
-                Hi There!
+                Hi There! Sams
             </body>
         </html>`
     );
